563978478c9ac58847ed0ba069a507317abde9b0
Eric Hannafin <eric.hannafin@students.ittralee.ie>
2020-10-24 11:04:02 +0100

    24/10/20

--- Week06/Exercise01/Insurance.java
+++ Week06/Exercise01/Insurance.java
@@ -1,4 +1,35 @@
 package Week06.Exercise01;
 
 public class Insurance {
+
+    private String policyName;
+    private int policyID;
+
+    public Insurance(String policyName, int policyID) {
+        setPolicyName(policyName);
+        setPolicyID(policyID);
+    }
+
+    public String getPolicyName() {
+        return policyName;
+    }
+
+    public void setPolicyName(String policyName) {
+        this.policyName = policyName;
+    }
+
+    public int getPolicyID() {
+        return policyID;
+    }
+
+    public void setPolicyID(int policyID) {
+        this.policyID = policyID;
+    }
+
+    @Override
+    public String toString() {
+        return "\nInsurance" +
+                "\nPolicyName: " + getPolicyName() +
+                "\nPolicyID: " + getPolicyID();
+    }
 }
